5c3c929da337ccd1e5308412870bdaa803ebd1b3
nasimhuq <nasim.huq@gmail.com>
2018-12-25 23:07:52 -0500

    Support proposed JavaScript features with Babel Plugins

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
 class App extends React.Component {
+  state = {
+    count: 0
+  }
+
   render() {
-    debugger
+
     return (
       <div>
-        <h1>Hello World - from webpack-dev-server with</h1>
-        <ul>
-          <li> watch and compile </li>
-          <li> auto refresh browser page</li>
-          <li> open browser on dev run </li>
-        </ul>
+        <h1>Hello World</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={() => this.setState(state => ({ count: state.count + 1}))}>+</button>
+        <button onClick={() => this.setState(state => ({ count: state.count - 1}))}>-</button>
+
       </div>
     )
   }
