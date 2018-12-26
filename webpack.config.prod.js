993ebe4ba928f77a177eeabf35de248d6531f729
nasimhuq <nasim.huq@gmail.com>
2018-12-26 03:42:40 -0500

    Target sepecific browsers with babel-preset-env and the babel ployfill

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,8 @@
 const merge = require('webpack-merge')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const baseConfig = require('./webpack.config.base')
-const reportFilename = `Bundle-size-report-${(new Date).toISOString().replace(/[:/.]/g, '-')}.html`
-// const reportFilename = 'Bundle-size-report.html'
+// const reportFilename = `Bundle-size-report-${(new Date).toISOString().replace(/[:/.]/g, '-')}.html`
+const reportFilename = 'Bundle-size-report.html'
 module.exports = merge(baseConfig, {
   mode: 'production',
   plugins: [new BundleAnalyzerPlugin({
