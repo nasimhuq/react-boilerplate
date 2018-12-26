174d2bd323c5d303b50666e436ac438cb9e9bc87
nasimhuq <nasim.huq@gmail.com>
2018-12-25 22:40:26 -0500

    Serve a webpack bundle while developing with webpack-dev-server

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World Again!!</h1>
+    return (
+      <div>
+        <h1>Hello World - from webpack-dev-server with</h1>
+        <ul>
+          <li> watch and compile </li>
+          <li> auto refresh browser page</li>
+          <li> open browser on dev run </li>
+        </ul>
+      </div>
+    )
   }
 }
 export default App
