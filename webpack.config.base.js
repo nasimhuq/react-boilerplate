c4959f129edba15b3d4efa2f248d26754bcfb15a
nasimhuq <nasim.huq@gmail.com>
2018-12-25 23:30:58 -0500

    Automatically import CSS in javascript with webpack using style-loader and css-loader

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -18,6 +18,11 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
           plugins: ['@babel/plugin-proposal-class-properties']
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_module/
       }
     ]
   },
