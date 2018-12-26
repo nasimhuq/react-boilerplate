931e6ee90dc803228fb8af4dae3a0eb0450054ae
nasimhuq <nasim.huq@gmail.com>
2018-12-26 03:19:29 -0500

    Externalize Dependencies to be loaded via CDN with webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,17 @@
 const merge = require('webpack-merge')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const baseConfig = require('./webpack.config.base')
-
+const reportFilename = `Bundle-size-report-${(new Date).toISOString().replace(/[:/.]/g, '-')}.html`
+// const reportFilename = 'Bundle-size-report.html'
 module.exports = merge(baseConfig, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: false,
+    reportFilename: reportFilename
+  })],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
 })
