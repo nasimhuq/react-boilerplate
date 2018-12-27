1cc6e724727c8bf62dd31f9d104e1a203f438b9e
nasimhuq <nasim.huq@gmail.com>
2018-12-26 23:00:53 -0500

    Setup Jest global imports

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-testing-library'
-import 'jest-dom/extend-expect'
-import 'react-testing-library/cleanup-after-each'
+
 import App from './App'
 
 describe('App', () => {
